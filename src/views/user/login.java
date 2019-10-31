4d3e2ac0d063c7b7843ee36a629f2dcfc78dd818
José Olinda <joseolindad@yahoo.com.br>
2019-10-31 10:46:45 -0300

    Redução do tamanho da tela

--- src/views/user/login.java
+++ src/views/user/login.java
@@ -26,11 +26,11 @@ public class login extends javax.swing.JFrame {
     private void initComponents() {
 
         javax.swing.JPanel PainelRoot = new javax.swing.JPanel();
+        btnClose = new javax.swing.JLabel();
         psSenha = new javax.swing.JPasswordField();
         txUserMail = new javax.swing.JTextField();
         lbLinkCriarConta = new javax.swing.JLabel();
         lbEntrar = new javax.swing.JLabel();
-        btnClose = new javax.swing.JLabel();
         lbUserPass = new javax.swing.JLabel();
         lbUserMail = new javax.swing.JLabel();
         lbTituloLogin = new javax.swing.JLabel();
@@ -50,49 +50,50 @@ public class login extends javax.swing.JFrame {
         PainelRoot.setOpaque(false);
         PainelRoot.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        psSenha.setFont(new java.awt.Font("Leelawadee UI", 0, 18)); // NOI18N
+        btnClose.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 24)); // NOI18N
+        btnClose.setForeground(new java.awt.Color(255, 255, 255));
+        btnClose.setText("x");
+        btnClose.setAlignmentY(0.0F);
+        PainelRoot.add(btnClose, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 10, 20, 30));
+
+        psSenha.setFont(new java.awt.Font("Leelawadee UI", 0, 14)); // NOI18N
         psSenha.setForeground(new java.awt.Color(130, 70, 117));
         psSenha.setText("jPasswordField1");
         psSenha.setBorder(null);
         psSenha.setOpaque(false);
-        PainelRoot.add(psSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 630, 330, -1));
+        PainelRoot.add(psSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 430, 230, -1));
 
-        txUserMail.setFont(new java.awt.Font("Leelawadee UI", 0, 18)); // NOI18N
+        txUserMail.setFont(new java.awt.Font("Leelawadee UI", 0, 14)); // NOI18N
         txUserMail.setForeground(new java.awt.Color(130, 70, 117));
         txUserMail.setText("email@exemplo.com");
         txUserMail.setBorder(null);
         txUserMail.setOpaque(false);
-        PainelRoot.add(txUserMail, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 522, 330, 40));
+        PainelRoot.add(txUserMail, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 350, 220, 40));
 
-        lbLinkCriarConta.setFont(new java.awt.Font("Leelawadee UI", 1, 28)); // NOI18N
+        lbLinkCriarConta.setFont(new java.awt.Font("Leelawadee UI", 1, 18)); // NOI18N
         lbLinkCriarConta.setForeground(new java.awt.Color(102, 102, 102));
         lbLinkCriarConta.setText("Criar nova conta");
-        PainelRoot.add(lbLinkCriarConta, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 870, -1, -1));
+        PainelRoot.add(lbLinkCriarConta, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 600, -1, -1));
 
-        lbEntrar.setFont(new java.awt.Font("Leelawadee UI", 1, 48)); // NOI18N
+        lbEntrar.setFont(new java.awt.Font("Leelawadee UI", 1, 24)); // NOI18N
         lbEntrar.setForeground(new java.awt.Color(130, 70, 117));
         lbEntrar.setText("Entrar");
-        PainelRoot.add(lbEntrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 710, -1, -1));
+        PainelRoot.add(lbEntrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 500, -1, -1));
 
-        btnClose.setFont(new java.awt.Font("Arial Rounded MT Bold", 1, 32)); // NOI18N
-        btnClose.setForeground(new java.awt.Color(255, 255, 255));
-        btnClose.setText("x");
-        PainelRoot.add(btnClose, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 20, -1, 30));
-
-        lbUserPass.setFont(new java.awt.Font("Leelawadee UI", 0, 24)); // NOI18N
+        lbUserPass.setFont(new java.awt.Font("Leelawadee UI", 0, 16)); // NOI18N
         lbUserPass.setForeground(new java.awt.Color(102, 102, 102));
         lbUserPass.setText("Senha");
-        PainelRoot.add(lbUserPass, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 580, -1, -1));
+        PainelRoot.add(lbUserPass, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 410, -1, -1));
 
-        lbUserMail.setFont(new java.awt.Font("Leelawadee UI", 0, 24)); // NOI18N
+        lbUserMail.setFont(new java.awt.Font("Leelawadee UI", 0, 16)); // NOI18N
         lbUserMail.setForeground(new java.awt.Color(102, 102, 102));
         lbUserMail.setText("E-mail");
-        PainelRoot.add(lbUserMail, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 490, -1, -1));
+        PainelRoot.add(lbUserMail, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 340, -1, -1));
 
         lbTituloLogin.setFont(new java.awt.Font("Leelawadee UI", 1, 48)); // NOI18N
         lbTituloLogin.setForeground(new java.awt.Color(255, 255, 255));
         lbTituloLogin.setText("<html>\nMyWorld<br>\n<font size=\"5\">Entre. Sinta-se em casa.</font>\n</html>");
-        PainelRoot.add(lbTituloLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 220, -1, -1));
+        PainelRoot.add(lbTituloLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 140, -1, -1));
 
         BtnFazerLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/images/btn_login.png"))); // NOI18N
         BtnFazerLogin.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -103,7 +104,7 @@ public class login extends javax.swing.JFrame {
                 BtnFazerLoginMouseExited(evt);
             }
         });
-        PainelRoot.add(BtnFazerLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 690, -1, -1));
+        PainelRoot.add(BtnFazerLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 480, -1, -1));
 
         lbBGLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/images/login_screen.png"))); // NOI18N
         lbBGLogin.setToolTipText("");
